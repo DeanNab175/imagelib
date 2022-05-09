67293561dcdd0329b7cf39ad93360d58a26ba46a
DeanNab175 <f.jscript@gmail.com>
2022-05-09 08:47:59 +0400

    added rel noreferrer on link target blank

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
         <div>
           <h1>Imagelib</h1>
           <p>Search all your images.
-            <a href="https://pixabay.com/" target="_blank">
+            <a href="https://pixabay.com/" target="_blank" rel="noreferrer">
               Credit to <img src="https://pixabay.com/static/img/logo.png" alt="Pixabay" width="60" />
             </a>
           </p>
